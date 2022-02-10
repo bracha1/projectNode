f45286705e78c1e62feaf45b787e825ba7b55053
bracha1 <bracha4572@gmail.com>
2022-02-11 00:15:58 +0200

    save files

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const requestIp = require('request-ip');
+const { Log } = require("./collections/log");
 
 /**
- * TODO:
+ * TODO: 
  * Change this to the real connection string!
  */
-db = mongoose.createConnection("mongodb://")
+//db = mongoose.createConnection("mongodb://")
+db = mongoose.createConnection("mongodb://localhost/persons")
 
+//db = mongoose.createConnection("mongodb://mongo-t:fasdewr@157.245.75.184/persons?authSource=admin")
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -19,22 +23,65 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+//app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+console.log("middlw")
 
+//-----------middleware---------3&&4
+app.use(async function (req, res, next) {
+  try {
+
+    let typeReq = req.method;
+    let date = new Date();
+    let ip = requestIp.getClientIp(req)
+    let log = new Log({ typeReq: typeReq, ip: ip, date: date });
+    // save model to database
+    log.save(function (err, log) {
+      if (err) return console.error(err);
+      console.log(log + " saved to log collection.");
+    });
+    let numLogs = rateLimit(req);
+    if (numLogs >= 10) {
+      let now = new Date();
+      // let maxDate = await logs.find({}).sort({ date: -1 }).limit(1)
+      let maxDate = await Log.findOne({}).sort({ date: -1 })
+      let dif = now.getTime() - maxDate.date.getTime();
+
+      var Seconds = dif / 1000;
+      var Seconds_Between_Dates = Math.abs(Seconds);
+      let sec = 60 - Seconds_Between_Dates;
+      return res.json({ code: 200, data: "You can make up to ten requests per minute you have " + sec + " seconds left" })
+    }
+    else {
+      next()
+    }
+  } catch (error) {
+    res.send('not valid!!!')
+  }
+})
+async function rateLimit(req) {
+  let ip = requestIp.getClientIp(req);
+  let now = new Date();
+  let minutesToAdd = -1;
+  let minuteAgo = new Date(now.getTime() + minutesToAdd * 60000);
+  let logs = await Log.find({ ip: ip, date: { $gte: minuteAgo } })
+  let numLogs = logs.length;
+  return numLogs;
+}
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const {Person} = require('../collections/person');
-const {Profession} = require("../collections/profession"); 
+const fs = require('fs');
+const formidable = require('formidable');
+const http = require('http');
+const requestIp = require('request-ip');
+const multer = require('multer')
+
+
+const { Person } = require('../collections/person');
+const { Profession } = require("../collections/profession");
+const { Log } = require("../collections/log");
+
 router.get('/populate-mongo', async (req, res, next) => {
-    let {data: persons} = await axios.get("https://raw.githubusercontent.com/dominictarr/random-name/master/names.json");
-    let {data: professions} = await axios.get('https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/occupations.json');
+    let { data: persons } = await axios.get("https://raw.githubusercontent.com/dominictarr/random-name/master/names.json");
+    let { data: professions } = await axios.get('https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/occupations.json');
     professions = professions.occupations;
     professions = professions.map(p => ({
         name: p,
@@ -22,31 +31,31 @@ router.get('/populate-mongo', async (req, res, next) => {
         if (hasProfession) {
             let pro = await Profession.find().limit(1).skip(getRandomInt(0, professions.length));
             pro = pro[0];
-            Person.findOneAndUpdate({name: person}, {
+            Person.findOneAndUpdate({ name: person }, {
                 $set: {
                     name: person,
                     age: getRandomInt(21, 120),
                     profession: pro._id
                 }
-            }, {upsert: true}).then()
+            }, { upsert: true }).then()
         } else {
-            Person.findOneAndUpdate({name: person}, {
+            Person.findOneAndUpdate({ name: person }, {
                 $set: {
                     name: person,
                     age: getRandomInt(21, 120),
                     profession: null
                 }
-            }, {upsert: true}).then()
+            }, { upsert: true }).then()
         }
 
     }
 
-    return res.json({code: 200})
+    return res.json({ code: 200 })
 
 });
 router.get('/', async (req, res) => {
     let p = await Person.findOne();
-    return res.json({code: 200, data: p})
+    return res.json({ code: 200, data: p })
 })
 
 function getRandomInt(min, max) {
@@ -54,11 +63,142 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
+function getIpAndDate(req) {
+    let date = new Date();
+    let ip = requestIp.getClientIp(req)
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ipAndDate = ip + " " + hours + ":" + minutes
+    return ipAndDate;
+}
 
 router.get('/', async (req, res) => {
     let person = await Person.findOne();
     let profession = await Profession.findOne();
-    return res.json({code: 200, data: {person, profession}})
+    return res.json({ code: 200, data: { person, profession } })
 })
+//---ex1
+
+router.get('/getAvgAgeHasNoProfession', async (req, res) => {
+    let persons = await Person.find({ profession: null });
+    let avg = await persons.aggregate([
+        {
+            "$group": {
+                "_id": null,
+                "Average": { "$avg": "$age" }
+            }
+
+        }
+    ])
+    return res.json({ code: 200, data: { avg } })
+})
+
+
+//---ex2
+router.get('/getMaxSalaryProfession', async (req, res) => {
+    let profession = await Profession.findOne({}).sort({ salary: -1 })
+    let persons = await Person.find({ profession: profession._id })
+    let numPersons = persons.length
+
+    return res.json({ code: 200, data: profession, numPersons })
+})
+
+
+
+// router.get('/getAvgHasNoProfessio', async (req, res) => {
+//     let professionMaxSalary = await Profession.findOne({ $max: '$salary' });
+//     // let numPersons = await Person.find({ profession: professionMaxSalary._id }).length;
+//     // return res.json({ code: 200, data: { professionMaxSalary, numPersons } });
+//     return res.json({ code: 200, data: { professionMaxSalary } });
+
+// })
+//---ex3
+
+router.get('/get5Proffession', async (req, res) => {
+    let maxSalaries = await Profession.find({}).sort({ salary: -1 }).limit(5)
+    return res.json({ code: 200, data: { maxSalaries } })
+
+})
+
+//---ex4
+router.get('/getDistinctAge', async (req, res) => {
+    let distinctAges = await Person.distinct('age');
+    return res.json({ code: 200, data: { distinctAges } });
+})
+//---ex5
+
+router.patch('/updateSalary', async (req, res) => {
+    var t = req.method;
+    let person = await Person.findOne({ name: req.body.name })
+    let profession;
+    if (person.profession != null) {
+        profession = await Profession.findByIdAndUpdate(person.profession, { salary: req.body.salary }, { new: true })
+        return res.json({ code: 200, data: { person, profession, t } });
+    }
+    else {
+        return res.json({ code: 200, data: "not have profession" });
+    }
+
+})
+
+//----------node
+//----------1
+
+router.get('/writeIpAndDate', async (req, res) => {
+    let date = new Date();
+    let ip = requestIp.getClientIp(req)
+    fs.appendFile('ipFile.txt', ip + " " + date + '\r\n', (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        return res.json({ code: 200, data: { ip, date } })
+    })
+
+})
+
+//-------2
+
+
+/////upload image//////
+const imageStorage = multer.diskStorage({
+    // Destination to store image     
+    destination: 'public/',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '_' + Date.now()
+            + path.extname(file.originalname))
+        // file.fieldname is name of the field (image)
+        // path.extname get the uploaded file extension
+    }
+});
+
+const imageUpload = multer({
+    storage: imageStorage,
+    limits: {
+        fileSize: 1000000 // 1000000 Bytes = 1 MB
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg)$/)) {
+            // upload only png and jpg format
+            let ip = requsetIp.getClientIp(req)
+            fs.appendFile('file2.txt', ip + " " + file.originalname + '\r\n', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+            })
+            return cb(new Error('Please upload a Image'))
+        }
+        cb(undefined, true)
+    }
+})
+
+// For Single image upload
+router.post('/uploadImage', imageUpload.single('image'), (req, res) => {
+    res.send(req.file)
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+
 
 module.exports = router;
